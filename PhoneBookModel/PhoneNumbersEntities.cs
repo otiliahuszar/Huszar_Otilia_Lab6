f3cc397dc8d1567944ef28f515122dd93dc86ec2
Otilia Huszar <otilia.huszar@pitechnologies.ro>
2020-12-21 13:51:58 +0200

    Laborator 6 - Phone Numbers

---
+++ PhoneBookModel/PhoneNumbersEntities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookModel
+{
+    public partial class PhoneNumbersEntities
+    {
+        public void SaveChangesClientWins()
+        {
+            CommitChanges(ClientWins: true);
+        }
+
+        public void SaveChangesDatabaseStoreWins()
+        {
+            CommitChanges(ClientWins: false);
+        }
+
+        private void CommitChanges(bool ClientWins = true)
+        {
+            bool saveFailed;
+            do
+            {
+                saveFailed = false;
+                try
+                {
+                    this.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    saveFailed = true;
+                    foreach (var entry in ex.Entries.Where(e => e.State != EntityState.Unchanged))
+                    {
+                        if (ClientWins)
+                        {
+                            DbPropertyValues dbValues = entry.GetDatabaseValues();
+                            if (dbValues != null)
+                            {
+                                entry.OriginalValues.SetValues(dbValues);
+                            }
+                            else
+                            {
+                                entry.State = EntityState.Added;
+                            }
+                        }
+                        else
+                        {
+                            entry.Reload();
+                        }
+                    }
+                }
+            } while (saveFailed);
+        }
+    }
+}
